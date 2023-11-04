64f6aba603ead482c09f999e9b8de5ebe59a2016
Julien MICHEL <julien.michel@student-cs.fr>
2023-11-04 17:16:50 +0000

    new files created to prepare fro Kstreams implementation

---
+++ 3MD1510_IAL-main-Tweetoscope/Tweetoscope/InitialTweetoscope/src/main/java/tweetoscope/tweetsFilter/SizeTweetFilter2.java
@@ -0,0 +1,27 @@
+package tweetoscope.tweetsFilter;
+
+import com.twitter.clientlib.model.Tweet;
+
+public class SizeTweetFilter2 extends TweetFilter {
+
+	/**
+	 * target language to match (examples: "fr", "en"...)
+	 */
+	protected int n;
+
+	/**
+	 * Creates a filter that tests whether the "language" tag of a Tweet (if it is
+	 * set) equals a given code.
+	 * 
+	 * @param language target language to match (example: "en")
+	 */
+	public SizeTweetFilter2(int n) {
+		this.n = n;
+	}
+
+	@Override
+	protected boolean match(Tweet tweet) {
+		return tweet.getText().length() >= n;
+	}
+
+}
