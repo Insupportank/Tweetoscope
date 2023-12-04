b073c753b0ca71b3524e3052b7d9fd88aa0f1e01
Aaron <aaron.broderick@student-cs.fr>
2023-12-04 22:20:49 +0100

    dockerfile for broker done, mooved dockerfiles in folders

--- topics.sh
+++ topics.sh
@@ -1,15 +1,14 @@
 echo "delete tweets topic if it exists"
-$KAFKA_HOME/bin/kafka-topics.sh --delete --if-exists --bootstrap-server localhost:9092 --topic tweets
+$KAFKA_HOME/bin/kafka-topics.sh --delete --if-exists --bootstrap-server localhost:9092,localhost:9093 --topic tweets
 echo "create tweets topic"
-$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --partitions 2 --config retention.ms=3600000 --topic tweets
+$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server localhost:9092,localhost:9093 --replication-factor 1 --partitions 2 --topic tweets
 
 echo "delete filtered_tweet topic if it exists"
-$KAFKA_HOME/bin/kafka-topics.sh --delete --if-exists --bootstrap-server localhost:9092 --topic filtered_tweet
+$KAFKA_HOME/bin/kafka-topics.sh --delete --if-exists --bootstrap-server localhost:9092,localhost:9093 --topic filtered_tweet
 echo "create filtered_tweet topic"
-$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --partitions 2 --config retention.ms=3600000 --topic filtered_tweet
+$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server localhost:9092,localhost:9093 --replication-factor 1 --partitions 2 --topic filtered_tweet
 
 echo "delete hashtags topic if it exists"
-$KAFKA_HOME/bin/kafka-topics.sh --delete --if-exists --bootstrap-server localhost:9092 --topic hashtags
+$KAFKA_HOME/bin/kafka-topics.sh --delete --if-exists --bootstrap-server localhost:9092,localhost:9093 --topic hashtags
 echo "create hashtags topic"
-$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server localhost:9092 --partitions 2 --config retention.ms=3600000 --topic hashtags
-
+$KAFKA_HOME/bin/kafka-topics.sh --create --bootstrap-server localhost:9092,localhost:9093 --replication-factor 1 --partitions 2 --topic hashtags
